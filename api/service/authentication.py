1b30a7c2fef7003afb466bc9cc08a89c7bf667be
Juliano Lopes <julopys@hotmail.com>
2024-07-04 01:22:17 -0300

    Removes unused dependencies.

--- api/service/authentication.py
+++ api/service/authentication.py
@@ -1,8 +1,5 @@
 import google.auth
 from google.cloud import storage
-from google.cloud import translate_v2 as translate
-from google.cloud import speech 
-from google.cloud import texttospeech
 
 class Authentication:
     def __init__(self, credential_key, bucket_name):
@@ -14,9 +11,6 @@ class Authentication:
         bucket = self.storage_client.bucket(bucket_name)
         self.bucket_name = bucket_name
         self.bucket = bucket
-        self.translate_client = translate.Client(credentials=credentials)
-        self.speech_client = speech.SpeechClient(credentials=credentials)
-        self.tts_client = texttospeech.TextToSpeechClient(credentials=credentials)
 
     def get_bucket(self):
         return self.bucket
